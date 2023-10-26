be03d5647886410d4c85c5bd1d89a1bc72e35ed8
emiliagit <emisosa2002@gmail.com>
2023-10-26 12:08:46 -0300

    armado de juego

---
+++ Assets/scripts/CamaraMovement.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamaraMovement : MonoBehaviour
+{
+    [SerializeField] private float mouseSensitivity = 50f;
+
+    [SerializeField] private Transform playerBody;
+
+    private float xRotation = 0f;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        Rotation();
+    }
+
+    private void Rotation()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+
+        playerBody.Rotate(Vector3.up * mouseX * 1.2f);
+    }
+}
---
+++ Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float VelocidadDesplazamiento = 10.0f;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float MovAdelanteAtras = Input.GetAxis("Vertical") * VelocidadDesplazamiento;
+        float MovIzqDer = Input.GetAxis("Horizontal") * VelocidadDesplazamiento;
+
+        MovAdelanteAtras *= Time.deltaTime;
+        MovIzqDer *= Time.deltaTime;
+
+        transform.Translate(MovIzqDer, 0, MovAdelanteAtras);
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+}
