5fae67ce295142f12526fe36a5d16fbd3a8e96ac
emiliagit <emisosa2002@gmail.com>
2023-10-29 19:13:16 -0300

    spawn interval

--- Assets/EnemyMomement.cs
+++ Assets/EnemyMomement.cs
@@ -13,10 +13,10 @@ public class EnemyMomement : MonoBehaviour
 
     //spawn de enemigos en mapa
     public GameObject enemyPrefab; // El prefabricado del enemigo 
-    public float spawnInterval = 2.0f; // El intervalo de tiempo entre cada generación de enemigos
+    public float spawnInterval = 10.0f; // El intervalo de tiempo entre cada generación de enemigos
     public float spawnRadius = 10.0f;
 
-    // private float timeSinceLastSpawn = 0.0f;
+    private float timeSinceLastSpawn = 0.0f;
 
 
     //datos para daño de jugador
@@ -34,30 +34,30 @@ public class EnemyMomement : MonoBehaviour
     {
         {
 
-            //// Verificar si tenemos una referencia al jugador
-            //if (jugador == null)
-            //{
-            //    Debug.LogWarning("No se ha asignado una referencia al jugador.");
-            //    return;
-            //}
+            // Verificar si tenemos una referencia al jugador
+            if (jugador == null)
+            {
+                Debug.LogWarning("No se ha asignado una referencia al jugador.");
+                return;
+            }
 
-            //// Calcular la dirección hacia el jugador
-            //Vector3 direccion = jugador.position - transform.position;
-            //direccion.Normalize(); // Normalizar para obtener una dirección unitaria
+            // Calcular la dirección hacia el jugador
+            Vector3 direccion = jugador.position - transform.position;
+            direccion.Normalize(); // Normalizar para obtener una dirección unitaria
 
-            //// Mover al enemigo en la dirección del jugador
-            //transform.Translate(direccion * velocidad * Time.deltaTime, Space.World); // Usar Space.World para mover en el mundo
+            // Mover al enemigo en la dirección del jugador
+            transform.Translate(direccion * velocidad * Time.deltaTime, Space.World); // Usar Space.World para mover en el mundo
 
-            //transform.LookAt(jugador); //hace que mire al jugador
+            transform.LookAt(jugador); //hace que mire al jugador
 
 
-            //timeSinceLastSpawn += Time.deltaTime;
+            timeSinceLastSpawn += Time.deltaTime;
 
-            //if (timeSinceLastSpawn >= spawnInterval)
-            //{
-            //    SpawnEnemy();
-            //    timeSinceLastSpawn = 0.0f;
-            //}
+            if (timeSinceLastSpawn >= spawnInterval)
+            {
+                SpawnEnemy();
+                timeSinceLastSpawn = 0.0f;
+            }
         }
     }
 
@@ -90,11 +90,11 @@ public class EnemyMomement : MonoBehaviour
 
 
 
-    //void SpawnEnemy()
-    //{
-    //    Vector3 randomPosition = transform.position + new Vector3(Random.Range(-spawnRadius, spawnRadius), 0, Random.Range(-spawnRadius, spawnRadius));
-    //    GameObject newEnemy = Instantiate(enemyPrefab, randomPosition, Quaternion.identity);
-    //}
+    void SpawnEnemy()
+    {
+        Vector3 randomPosition = transform.position + new Vector3(Random.Range(-spawnRadius, spawnRadius), 0, Random.Range(-spawnRadius, spawnRadius));
+        GameObject newEnemy = Instantiate(enemyPrefab, randomPosition, Quaternion.identity);
+    }
 
 
 
