8910f1c15976bec974497cc433f5bb23a740ad6c
emiliagit <emisosa2002@gmail.com>
2023-10-30 15:17:54 -0300

    timer

--- Assets/timer.cs
+++ Assets/timer.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 
 public class timer : MonoBehaviour
 {
-     private int min = 1;
+     private int min = 1;  
      private int seg = 30;
    
     [SerializeField] TextMeshProUGUI tiempo;
